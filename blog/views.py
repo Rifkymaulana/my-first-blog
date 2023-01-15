dc0725be000971b45c9e8ae3cdeac2be8826b285
rifkymaulana <rifkymaulanaindonesia@gmail.com>
2023-01-15 12:41:21 +0700

    change queryset in blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.order_by('published_date')
+    posts = Post.objects.order_by('published_date').filter(published_date__lte=timezone.now())
     # posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts':posts})
